d39556f7afdfd34f012fbaf19216bf6b1900da72
Mikayla <mikayla@thehubbards.org>
2026-01-31 20:12:50 -0500

    refactor using encapsulation

--- Pong/Assets/Scripts/BallMovement.cs
+++ Pong/Assets/Scripts/BallMovement.cs
@@ -1,18 +1,58 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class BallMovement : MonoBehaviour
 {
+    private float speed = 7f;
+    private float maxSpeed = 20f;
+    private Vector2 direction;
+    private Rigidbody2D ball;
+
+
+    public float Speed
+    {
+        get { return speed; }
+        set
+        {
+            if (value > maxSpeed)
+            {
+                speed = maxSpeed;
+            }
+            else if (value < 0)
+            {
+                speed = 0;
+            }
+            else
+            {
+                speed = value;
+            }
+        }
+    }
+
+    public Vector2 Direction
+    {
+        get { return direction; }
+        set
+        {
+            direction = value.normalized;
+        }
+    }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         // Get components
-        Rigidbody2D ball = GetComponent<Rigidbody2D>();
+        ball = GetComponent<Rigidbody2D>();
 
         // Set initial values
-        ball.linearVelocity = new Vector2(3.0f, 3.0f);
+        Direction = new Vector2(3.0f, 3.0f);
 
     }
 
+    void FixedUpdate()
+    {
+        ball.linearVelocity = direction * speed;
+    }
     // Update is called once per frame
     void Update()
     {
@@ -21,18 +61,15 @@ public class BallMovement : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        Rigidbody2D ball = GetComponent<Rigidbody2D>();
-
         if (collision.gameObject.tag == "vertical")
         {
-            ball.linearVelocity = new Vector2(-ball.linearVelocity.x, ball.linearVelocity.y);
+            Direction = new Vector2(-Direction.x, Direction.y);
         }
         else if (collision.gameObject.tag == "horizontal")
         {
-            ball.linearVelocity = new Vector2(ball.linearVelocity.x, -ball.linearVelocity.y);
+            Direction = new Vector2(Direction.x, -Direction.y);
         }
+
+        ball.linearVelocity = direction * speed;
     }
-
-
-
 }
